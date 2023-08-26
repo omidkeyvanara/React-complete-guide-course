f89b006aedf5c0320911083cd6cac23dcc774a36
omid01~ <omid.amin90@gmail.com>
2023-08-26 07:58:21 -0700

    first commit

---
+++ react-complete-guide-code-02-js-refresher/code/finished/assets/scripts/App.js
@@ -0,0 +1,21 @@
+const names = [`a`, `b`, `c`, `d`];
+
+console.log(names.indexOf());
+
+
+
+
+
+const index = names.findIndex((i) => i === `a`);
+
+console.log();
+
+const newNames = names.map((item) => item + names.indexOf());
+console.log(newNames);
+
+
+
+
+const finder = function (x) {
+    return 
+} 
